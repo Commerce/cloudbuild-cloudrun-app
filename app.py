cb89467c14e951a87c7152e5cc52ffe447b83788
jevon.gordon@gmail.com <jevon.gordon@gmail.com>
2025-09-26 21:21:54 +0000

    Update app message with project ID and trigger build

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_cloud_run():
-    return f'Hello from Cloud Run! This is deployment version {os.environ.get("BUILD_ID", "local")}.'
+    return f'Hello from Cloud Run! This is deployment version {os.environ.get("BUILD_ID", "local")}. Project: Aphrodite-66393.' # Added project ID to message
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
